0e868bfb3a0a97a1150bcff98c97ca5aa787a146
rodrigo.tomiosso <rodrigobt20@gmail.com>
2020-08-12 17:42:06 -0300

    [feature/apiConfiguration][WIP] Creates Images Model

---
+++ tmdb/tmdb/Model/Images.swift
@@ -0,0 +1,21 @@
+//  Images.swift
+//  tmdb
+
+import Foundation
+
+struct Images: Codable {
+    let baseURL: String
+    let secureBaseURL: String
+    let backdropSizes, logoSizes, posterSizes, profileSizes: [String]
+    let stillSizes: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case baseURL = "base_url"
+        case secureBaseURL = "secure_base_url"
+        case backdropSizes = "backdrop_sizes"
+        case logoSizes = "logo_sizes"
+        case posterSizes = "poster_sizes"
+        case profileSizes = "profile_sizes"
+        case stillSizes = "still_sizes"
+    }
+}
