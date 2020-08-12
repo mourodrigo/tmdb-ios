08d750cbaf1c8be24fc461bcbd771c21414a3efa
rodrigo.tomiosso <rodrigobt20@gmail.com>
2020-08-12 17:43:40 -0300

    [feature/apiConfiguration][WIP] Genres Model

---
+++ tmdb/tmdb/Model/Genres.swift
@@ -0,0 +1,23 @@
+//  Genres.swift
+//  tmdb
+
+import Foundation
+
+struct Genre: BiCodable {
+  let id: Int
+  let name: String
+}
+
+struct Genres: BiCodable { //For API Response
+  let values: [Genre]
+
+  enum CodingKeys: String, CodingKey {
+    case values = "genres"
+  }
+}
+
+enum APIResponse {
+    case success
+    case loading
+    case error(error: Error)
+}
