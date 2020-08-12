9b2f0c32f111b8336bd4ae3306ee44dd310bcbf5
rodrigo.tomiosso <rodrigobt20@gmail.com>
2020-08-12 17:22:03 -0300

    [feature/apiConfiguration][WIP] Creates data extension for encoding objects from API

---
+++ tmdb/tmdb/Utils/Extensions/Data+.swift
@@ -0,0 +1,14 @@
+//  Data+.swift
+//  tmdb
+
+import Foundation
+
+extension Data {
+    func mapObject<T: Codable>(_ resultType: T.Type) -> T {
+        do {
+            return try JSONDecoder().decode(T.self, from: self)
+        } catch let error {
+            return error as! T
+        }
+    }
+}
