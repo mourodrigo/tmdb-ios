fe893a2a30a3524bec255d6a021a81aba9f15647
rodrigo.tomiosso <rodrigobt20@gmail.com>
2020-08-20 15:12:24 -0300

    [feature/movieDetailsInfo] Creates Video model

---
+++ tmdb/tmdb/Model/Video.swift
@@ -0,0 +1,22 @@
+//  Video.swift
+//  tmdb
+import Foundation
+
+struct VideoResponse: Codable {
+    let id: Int
+    let results: [Video]
+}
+
+struct Video: Codable {
+    let id, iso639_1, iso3166_1, key: String
+    let name, site: String
+    let size: Int
+    let type: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case iso639_1 = "iso_639_1"
+        case iso3166_1 = "iso_3166_1"
+        case key, name, site, size, type
+    }
+}
